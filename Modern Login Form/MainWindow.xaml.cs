dba302b6f20d90de37f659f6d62d36dc54c98f7a
Ibrahim <ibrahimasadov32@gmail.com>
2023-07-05 21:09:16 +0400

    commit

---
+++ Modern Login Form/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Modern_Login_Form
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void textEmail_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtEmail.Focus();
+        }
+        private void txtEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtEmail.Text) && txtEmail.Text.Length > 0)
+            {
+                textEmail.Visibility = Visibility.Collapsed;
+            }
+            else if (string.IsNullOrEmpty(txtEmail.Text))
+            {
+                textEmail.Visibility = Visibility.Visible;
+            }
+            else { txtEmail.Visibility = Visibility.Visible; }
+        }
+        private void textPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtPassword.Focus();
+        }
+        private void txtPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtPassword.Text) && txtPassword.Text.Length > 0)
+            {
+                textPassword.Visibility = Visibility.Collapsed;
+            }
+            else if (string.IsNullOrEmpty(txtPassword.Text))
+            {
+                textPassword.Visibility = Visibility.Visible;
+            }
+            else { txtPassword.Visibility = Visibility.Visible; }
+        }
+
+        private void signInButtonClick(object sender, RoutedEventArgs e)
+        {
+            string email= txtEmail.Text;
+            string password= txtPassword.Text;
+          
+            if (!txtEmail.Text.EndsWith("@gmail.com") || txtPassword.Text.Length < 7)
+            {
+                MessageBox.Show("Invalid Email Or Password!");
+            }
+            else
+            {
+                SignIn signIn = new SignIn(email,password);
+                App.Current.MainWindow.Close();
+                signIn.Show();
+
+            }
+        }
+    }
+}
---
+++ Modern Login Form/SignIn.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Modern_Login_Form
+{
+    
+    public partial class SignIn : Window
+    {
+        public SignIn(string email,string password)
+        {
+            InitializeComponent();
+            DataContext = new SignInViewModel(email, password);
+        }
+        public class SignInViewModel
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
+
+            public SignInViewModel(string email, string password)
+            {
+                Email = email;
+                Password = password;
+            }
+        }
+    }
+}
